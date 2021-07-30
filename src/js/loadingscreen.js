a7ee922e0ef841dbf44955d96cbde3039d7b179a
yudasetyo <yudasetyo07@gmail.com>
2021-07-30 14:31:02 +0700

    thirteen commit

---
+++ src/js/loadingscreen.js
@@ -0,0 +1,7 @@
+
+const loader = document.getElementById('loading-screen')
+
+// Once page is finished loading start the transition to page content. Set timeout is only used here to simulate a page load time of 3 seconds.
+window.addEventListener('load', function(e) {
+    setTimeout(() => { loader.classList.replace('opacity-100', 'opacity-0'); }, 500);
+})
