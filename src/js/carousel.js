2a470f92f1e5554ea4307a1de6d86c38e47d3dd4
yudasetyo <yudasetyo07@gmail.com>
2021-07-30 13:05:20 +0700

    twelve commit

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,57 +1,50 @@
 var cont=0;
     function loopSlider(){
-      var xx= setInterval(function(){
-          switch(cont)
-          {
-          case 0:{
-              $("#slider-1").fadeOut(400);
-              $("#slider-2").delay(400).fadeIn(400);
-              $("#Button1").removeClass("bg-purple-800");
-              $("#Button2").addClass("bg-purple-800");
-          cont=1;
-          break;
-          }
-          case 1:
-          {
-          
-              $("#slider-2").fadeOut(400);
-              $("#slider-1").delay(400).fadeIn(400);
-              $("#Button2").removeClass("bg-purple-800");
-              $("#Button1").addClass("bg-purple-800");
-             
-          cont=0;        
-          break;
-          }
-            
-          }},5000);
+        var xx= setInterval(function(){
+            switch(cont)
+            {
+                case 0:{
+                    $("#slider-1").fadeOut(400);
+                    $("#slider-2").delay(400).fadeIn(400);
+                    $("#Button1").removeClass("bg-purple-800");
+                    $("#Button2").addClass("bg-purple-800");
+                cont=1;
+                break;
+                }
+                case 1:
+                {
+                    $("#slider-2").fadeOut(400);
+                    $("#slider-1").delay(400).fadeIn(400);
+                    $("#Button2").removeClass("bg-purple-800");
+                    $("#Button1").addClass("bg-purple-800");
+                cont=0;        
+                break;
+                }
+            }
+        },5000);
     }
-
     function reinitLoop(time){
     clearInterval(xx);
     setTimeout(loopSlider(),time);
     }
-
-
     function sliderButton1(){
-      $("#slider-2").fadeOut(400);
-      $("#slider-1").delay(400).fadeIn(400);
-      $("#Button2").removeClass("bg-purple-800");
-      $("#Button1").addClass("bg-purple-800");
-      reinitLoop(4000);
-      cont=0
-      }
-    
-      function sliderButton2(){
-      $("#slider-1").fadeOut(400);
-      $("#slider-2").delay(400).fadeIn(400);
-      $("#Button1").removeClass("bg-purple-800");
-      $("#Button2").addClass("bg-purple-800");
-      reinitLoop(4000);
-      cont=1
-      }
-
-      $(window).ready(function(){
-        $("#slider-2").hide();
-        $("#Button1").addClass("bg-purple-800");
+            $("#slider-2").fadeOut(400);
+            $("#slider-1").delay(400).fadeIn(400);
+            $("#Button2").removeClass("bg-purple-800");
+            $("#Button1").addClass("bg-purple-800");
+        reinitLoop(4000);
+        cont=0
+        } 
+        function sliderButton2(){
+            $("#slider-1").fadeOut(400);
+            $("#slider-2").delay(400).fadeIn(400);
+            $("#Button1").removeClass("bg-purple-800");
+            $("#Button2").addClass("bg-purple-800");
+        reinitLoop(4000);
+        cont=1
+        }
+        $(window).ready(function(){
+            $("#slider-2").hide();
+            $("#Button1").addClass("bg-purple-800");
         loopSlider();
     });
